f15a8762da64633a0fa999a918b75b2c68cff865
smclaren727 <smclaren727@gmail.com>
2015-06-16 12:37:02 -0400

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,25 @@ posts = Post.all
     )
 end
 
+# Create a custom post and check to make sure it doesn't already exist
+post = [
+  { title: "Test Title", body: "Sample test for the body" }
+]
+
+post.each do |post|
+  Post.where(post).first_or_create
+end
+
+# Create a custom comment and check to make sure it doesn't already exist
+comment = [
+  { post: "This is a test post", body: "Sample test for the body of the comment" }
+]
+
+comment.each do |comment|
+  Comment.where(comment).first_or_create
+end
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
