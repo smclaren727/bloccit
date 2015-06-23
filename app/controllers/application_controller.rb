54fcbf3674f30876362f31fc56540d64aa5febb7
smclaren727 <smclaren727@gmail.com>
2015-06-23 11:09:28 -0400

    Checkpoint 38 complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sing_up) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,23 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 # Create Posts
 50.times do
   Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
     )
 end
 posts = Post.all
@@ -12,11 +25,20 @@ posts = Post.all
 # Create Comments
 100.times do
   Comment.create!(
+    # user: users.sample, #we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
     )
 end
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'smclaren727@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
