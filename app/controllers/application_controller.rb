5d2a4bd275e695a647b26d9f69bcf33ee66564bb
smclaren727 <smclaren727@gmail.com>
2015-06-22 22:35:13 -0400

    Assignment 37 completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :flash_attack
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sing_up) << :name
+  end
+
+  def flash_attack
+    flash[:notice] = "Flash Attack"
   end
 end
