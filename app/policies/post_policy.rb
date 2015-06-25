5e48db90d91a9c4c26701ffa9b473dbdac19d66b
smclaren727 <smclaren727@gmail.com>
2015-06-25 17:29:08 -0400

    Assignment 39 complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,25 @@
 class PostPolicy < ApplicationPolicy
 
+  class Scope < Scope
+    def resolve
+      if user.present?
+        if user.admin? || user.moderator? 
+          scope.all
+        else
+          scope.where(:published => true)
+        end 
+      else
+        scope
+      end
+    end
+  end
+
   def index?
-    true
+    user.present?
+  end
+
+  def update?
+    user.admin? or not post.published?
   end
   
 end
