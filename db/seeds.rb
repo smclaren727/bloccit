9803c8e66dee559b271c7bcb11b8880cda936a73
smclaren727 <smclaren727@gmail.com>
2015-06-30 18:03:17 -0400

    Assignment 45 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ end
 users = User.all
 
 # Create Topics
-15.times do
+100.times do
   Topic.create!(
     name:         Faker::Lorem.sentence, 
     description:  Faker::Lorem.paragraph 
@@ -22,7 +22,7 @@ end
 topics = Topic.all
 
 # Create Posts
-50.times do
+1000.times do
   Post.create!(
     user:   users.sample,
     topic:  topics.sample,
