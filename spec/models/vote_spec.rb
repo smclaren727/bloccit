ff893caff3b8980a9ba2dc77f8e37b1a6680cd65
smclaren727 <smclaren727@gmail.com>
2015-07-02 17:10:09 -0400

    Assignment 48 complete

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,14 @@
+describe Vote do 
+  describe "validations" do
+    before do
+      v1 = Vote.new(value: 1)
+      v2 = Vote.new(value: -1)
+      v3 = Vote.new(value: 2)
+    end  
+    it "only allows -1 or 1 as values" do
+      expect(v1.valid?).to eq(true)
+      expect(v2.valid?).to eq(true)
+      expect(v3.valid?).to eq(false)
+    end
+  end   
+end
