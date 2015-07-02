2043d74f3b7ef3e70e2a6699043f74bee7aaa4d7
smclaren727 <smclaren727@gmail.com>
2015-07-02 13:01:41 -0400

    Assignment 47 complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,43 +2,36 @@ class CommentsController < ApplicationController
   def new
    @post = Post.find(params[:post_id])
    @comment = Comment.new
-   @comment.post = @post
-   @topic = @comment.post.topic
    authorize @comment
   end
   
   def create
      @post = Post.find(params[:post_id])
      @comments = @post.comments
-     @comment = @post.comments.new(comment_params)
-     @comment.user = current_user
-     @topic = @comment.post.topic
+     @comment = current_user.comments.build(comment_params)
+     @comment.post = @post
      authorize @comment
      if @comment.save
        flash[:notice] = "Comment was saved."
-       redirect_to [@topic, @comment.post]
+       redirect_to [@post.topic, @post]
      else
        flash[:error] = "There was an error saving the comment. Please try again."
        render "posts/show" 
-     end
-  end
+     end      
+   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
- 
-    authorize @comment
-    if @comment.destroy
+     @post = Post.find(params[:post_id])
+     @comment = Comment.find(params[:id])
+     authorize @comment
+     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
-    else
-      flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
-    end
+       redirect_to [@post.topic, @post]
+     else
+       flash[:error] = "There was an error removing the comment. Please try again."
+       render 'posts/show'
+     end
   end
-
-private
 
   def comment_params
      params.require(:comment).permit(:body)
