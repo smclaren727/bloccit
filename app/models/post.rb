1b17a5ccd2a8a3f2e8fe0479f4e8d493d9840169
smclaren727 <smclaren727@gmail.com>
2015-07-05 22:07:38 -0400

    Assignment 49 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,7 @@ class Post < ActiveRecord::Base
   validates :body, length: {minimum: 20 }, presence: true
   #validates :topic, presence: true
   #validates :user, presence: true
+  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -29,4 +30,11 @@ class Post < ActiveRecord::Base
 
     update_attribute(:rank, new_rank)
   end
+
+private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+  
 end
