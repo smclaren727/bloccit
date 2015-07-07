a2fe0703a0068304a90b29b734faa9868a2d7298
smclaren727 <smclaren727@gmail.com>
2015-07-07 09:35:16 -0400

    Rspec load error - working on solution

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,29 +1,41 @@
 require 'rails_helper'
 
-describe Post do
+describe  Post  do 
+
+  include TestFactories
+
   describe "vote methods" do
-
     before do
-      @post = Post.create(title: 'Post title', body: 'Post bodies must be pretty long.')
-      3.times { @post.votes.create(value: 1) }
-      2.times { @post.votes.create(value: -1) }
+      @post = associated_post
+      @post.save
+      3.times{@post.votes.create(value: 1)}
+      2.times{@post.votes.create(value: -1)}
     end
-
+    
     describe '#up_votes' do
-      it "counts the number of votes with value = 1" do
-        expect( @post.up_votes ).to eq(3)
+      it "countes number of votes with value =1" do
+        expect(@post.up_votes).to eq(3)
       end
     end
 
     describe '#down_votes' do
-      it "counts the number of votes with value = -1" do
-        expect( @post.down_votes ).to eq(2)
+      it "coutes number of votes with value = -1" do
+        expect(@post.down_votes).to eq(2)
       end
     end
 
     describe '#points' do
       it "returns the sum of all down and up votes" do
-        expect( @post.points ).to eq(1) # 3 - 2
+        expect(@post.points).to eq(1) # 3-2 =1
+      end
+    end    
+
+    describe "#create_vote" do
+      it "generates an up_vote when explicitly called" do
+        post = associated_post
+        expect(post.up_votes).to eq(0)
+        post.create_vote
+        expect(post.up_votes).to eq(1)
       end
     end
   end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+  include TestFactories
+
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        v1 = Vote.new(value: 1)
+        v2 = Vote.new(value: -1)
+        v3 = Vote.new(value: 2)
+        expect(v1.valid?).to eq(true)
+        expect(v2.valid?).to eq(true)
+        expect(v3.valid?).to eq(false)
+      end
+    end
+  end
+
+  describe "after_save" do
+    include TestFactories
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end  
 end
