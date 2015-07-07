f5852323c13f82a8a1f4e94002722a3cf4e6d985
smclaren727 <smclaren727@gmail.com>
2015-07-07 11:18:25 -0400

    Assignment 52 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,13 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, presence: true
+
+  self.per_page = 50
+
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+  scope :visible_to, -> (user) {user ? all : publicly_viewable}
+  
 end
