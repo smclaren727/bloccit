eab7d7fec195793b68ef933be31ec967ddd4df2b
smclaren727 <smclaren727@gmail.com>
2015-07-07 12:51:34 -0400

    Assignment 53 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+  def my_name
+    return "Fong Fan"
+  end
+
   def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
@@ -7,10 +12,12 @@ module ApplicationHelper
     end
   end
 
-  def markdown_to_html(markdown)
-    renderer = Redcarpet::Render::HTML.new
-    extensions = {fenced_code_blocks: true}
-    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    (redcarpet.render markdown).html_safe
+  def up_vote_link_classes(post)
+    (current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' 
   end
+
+  def down_vote_link_classes(post)
+    (current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' 
+  end
+
 end
