fcf7c247f297c7895de9659c3d9752881234a34e
smclaren727 <smclaren727@gmail.com>
2015-07-08 16:25:40 -0400

    Checkpoint 54 complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,8 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   #mount_uploader :image, ImageUploader
   default_scope { order('rank DESC') }
+  scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
+
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: {minimum: 20 }, presence: true
