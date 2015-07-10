22889e4e172febd17d8f745ccb550525ca30328a
smclaren727 <smclaren727@gmail.com>
2015-07-09 23:38:43 -0400

    Checkpoint 58 complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
     @comment.post = @post
+    @new_comment = Comment.new
 
     authorize @comment
     if @comment.save
@@ -33,6 +34,8 @@ class CommentsController < ApplicationController
       format.js
     end
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:body)
